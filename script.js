1109533776b8f27482c56ad0345e60d0ac43f1fd
Wilkson Flor Soares <90102289+Wilksonflor@users.noreply.github.com>
2023-01-08 21:06:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 function valorTotal() {
     let porcentagemGorjeta;
-    const valorConta = Number(document.getElementById('valorConta').value.replace(",","."));
+    const valorConta = Number(document.getElementById('valorConta').value.replace(",", "."));
     const prctGorjeta = Number(document.getElementById('porcentagem').value);
     const qntPessoas = Number(document.getElementById('qntPessoas').value);
     let gorjetaTotal = document.getElementById('gorjetaTotal');
     let somaTotal = document.getElementById('contaTotal');
-    let valorPessoa = parseFloat(document.getElementById('valorPessoa'));
+    let valorPessoa = document.getElementById('valorPessoa');
 
     porcentagem = prctGorjeta / 100;
 
     gorjetaTotal = valorConta * porcentagem;
-    console.log(gorjetaTotal);
+    document.getElementById('gorjetaTotal').value = parseFloat(gorjetaTotal).toFixed(2);
 
     somaTotal = valorConta + gorjetaTotal;
-    console.log(somaTotal);
-    
+    document.getElementById('contaTotal').value = parseFloat(somaTotal).toFixed(2);
 
     valorPessoa = somaTotal / qntPessoas;
-    console.log(valorPessoa);
-    
+    document.getElementById('valorPessoa').value = parseFloat(valorPessoa).toFixed(2);
+
 }
 
 const btnResultado = document.getElementById('btnCalcular');
 btnResultado.addEventListener('click', valorTotal);
 
 
+function limparForm() {
+    valorConta.value = '';
+
+}
+
+const btnReset = document.getElementById('reset')
+btnReset.addEventListener('click', limparForm)
